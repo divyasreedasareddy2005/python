6114ebb7119f9fe4c12157d884cbc996af1a1d35
divyasreedasareddy2005 <divyasreedasareddy2005@gmail.com>
2025-09-22 11:43:09 +0530

    practice

---
+++ practice/demo.py
@@ -0,0 +1,11 @@
+my__tuple =(2,"hello",(7,8,9),[12,13,14])
+
+my__tuple[3][2] = 35
+my__list = [10,20,30]
+my__list.reverse()
+print(my__list)
+
+valu =my__list.copy()
+print(valu)
+print
+print("hellow world")

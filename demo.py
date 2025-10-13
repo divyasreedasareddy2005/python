16a13a68d385f500632aab029b52c0504b06d64d
divyasreedasareddy2005 <divyasreedasareddy2005@gmail.com>
2025-10-13 11:18:26 +0530

    update

--- demo.py
+++ demo.py
@@ -31,10 +31,9 @@ def employe_info(employe_name,employe_email,employe_location,company_name):
     print(f"hi{employe_name},your email is {employe_email},ur working place is {employe_location}and ur working for {company_name}")
 employe_info(employe_location = "hyd",employe_name = "divya",employe_email =" divya@gmail.com", company_name = "meta")
 
-
 def emp_info(name,age,institute = "Edify"):
    print(institute)
-emp_info("divya","age","jupiter")
+emp_info("divya","20","jupiter")
 
 #dictnory
 
@@ -43,15 +42,16 @@ details["emp_name"] = "divya"
 details["emp_email_id"] = "divya@gmail.com"
 details["emp_location"]= "hyd"
 print(details)
-#global scope
 
+#global scope
 ga = 30
 def add(la,lb):
     print(la)
     print(lb)
     print(ga)
 add(20,30)
-#name conflict
+
+
 ga = 30
 def add(la,lb,ga):
     print(la)
@@ -181,3 +181,83 @@ print((lambda num:num%2==0)(5))
 #
 emp_info =((lambda emp_name,emp_email,emp_location:print(f"hi {emp_name}, ur email is {emp_email} and ur location is {emp_location}"))("divya","divya@gmail.com","hyd"))
 emp_info
+
+#regular functions
+
+def greet (name):
+   print("hello",name)
+   print("welcome to python")
+greet("divya")
+
+#normal
+def add_num(*numbers):
+    total=0
+    for num in numbers:
+      total+=num
+    return total 
+print(add_num(1,2,3))
+
+##without map
+def square_list(numbers):
+   square_list = []
+   for num in numbers:
+      square_list.append(num*num)
+      return square_list
+print(square_list([1,2,3,4,5]))
+#with map()
+print(list(map(lambda num:num*num,[1,2,3,4,5])))
+
+##
+print(map(lambda num:num*num,[1,2,3,4,5]))
+
+##without filter()
+def even_list(numbers):
+   even_numbers = []
+   for num in numbers:
+      if num % 2 == 0:
+         even_numbers.append(num)
+   return even_numbers
+
+print(even_list([1,2,3,4]))
+
+##with filters()
+print(filter(lambda num :num %2==0,[1,2,3,4,5]))
+
+print(list(filter(lambda num :num %2==0,[1,2,3])))
+
+##print(list(filter(lambda num : num+2=0,[1,3,4])))If you want to add 2 to each number in the list, use map instead of filter (since filter expects a function returning a boolean value)
+
+print(list(map(lambda num :num+2,[1,2,3]))) 
+
+##print(map(lambda num:num-2,1,2,3))map(function, iterable)
+#That means the second #argument must be a single iterable (like a list, tuple, or string) — not multiple numbers.
+
+
+print(list(map(lambda a, b: a + b, [1, 2, 3], [4, 5, 6])))
+
+##reduce without reduce
+
+def even_list(numbers):
+   even_numbers=[]
+   for num in numbers:
+      if num %2==0:
+       even_numbers.append(num)
+   return even_numbers
+print(even_list([2,3,4,6]))
+
+
+def sum_list(numbers):
+    sum_numbers = []
+    for num in numbers:
+        sum_numbers.append(num + 2)   # store the new number in the list
+    return sum_numbers                # return the result list
+
+print(sum_list([3, 5, 6, 2]))
+
+#with reduce()
+#frome functions import reduce
+#print(reduce(lambda result num:result*num,[12]))in Python, lambda parameters must be separated by a comma:
+
+#print(reduce(lambda result , num:result*num,[1,3,7]))NameError: name 'reduce' is not defind
+
+
